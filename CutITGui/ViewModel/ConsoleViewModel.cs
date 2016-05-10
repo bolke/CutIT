8bee545c8487b1fdbc26c7c172de3c4f5d2981e1
bolke <debolke@gmail.com>
2016-05-10 12:26:42 +0200

    Binding ConsoleOutput and Input to the view. Added commands to the model.

--- CutITGui/ViewModel/ConsoleViewModel.cs
+++ CutITGui/ViewModel/ConsoleViewModel.cs
@@ -1,20 +1,46 @@
 ﻿using CutIT.GRBL;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CutITGui.ViewModel
 {
     public class ConsoleViewModel:ViewModelBase
     {
+        ICommand _inputCommand = null;
+        string _consoleInput = "";
+        string _consoleOutput = "";
         TcpGrblClient _tcpGrblClient;
+        MessageViewModel _messageViewModel;
+
+        public string ConsoleInput {
+            get { return _consoleInput; }
+            set { Set("ConsoleInput",ref _consoleInput, value); }
+        }
+
+        public string ConsoleOutput
+        {
+            get { return _consoleOutput; }
+            set { Set("ConsoleOutput", ref _consoleOutput, value); }
+        }
 
         public ConsoleViewModel()
         {            
             _tcpGrblClient = ViewModelLocator.TcpGrblClient;
+            _messageViewModel = ViewModelLocator.MessageViewModel;
+        }        
+
+        public ICommand InputCommand { get { if (_inputCommand == null) _inputCommand = new RelayCommand(DoInputCommand); return _inputCommand; } }
+
+        public void DoInputCommand()
+        {
+            Console.WriteLine(_consoleInput);
+            ConsoleInput = "";
         }
     }
 }
